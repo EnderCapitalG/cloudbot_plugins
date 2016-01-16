a8fc30aa1403988f0a9fcc0e72593ec4e99ab94a
EnderCapitalG <ender.capitalg@gmail.com>
2016-01-16 07:31:56 -0500

    Overhauled evestatus.py, added local caching in memory (no more needing a cron job), temporarily using a local xml file to initialize global variable until I get un-dumb

--- plugins/evestatus.py
+++ plugins/evestatus.py
@@ -3,19 +3,56 @@
 
 from cloudbot import hook
 from lxml import etree
+import arrow
+import requests
+
+tqcache = 0
+sisicache = 0
+tqtree = etree.parse("data/tmp.xml")
+sisitree = etree.parse("data/tmp.xml")
 
 @hook.command('tq')
 @hook.command('eve')
 @hook.command('evestatus')
 def evestatus():
-	tree = etree.parse("data/ServerStatus.xml")
+	global tqcache, tqtree
+	now = arrow.utcnow()
+	if tqcache is 0 or now > tqcache:
+		url_tq = requests.get("https://api.eveonline.com/server/ServerStatus.xml.aspx")
+		tree = etree.fromstring(url_tq.content)
+		tqcache = arrow.get(tree.find("cachedUntil").text)
+		tqtree = tree
+		print("Parsing TQ server status. Seconds until cache:", (tqcache - now).seconds)
+
+	if tqtree.find("result/serverOpen").text == "True":
+        	online = "Online"
+	else:
+		pr = "Tranquility server status: Offline."
+		return pr
+
+	players = tqtree.find("result/onlinePlayers")
+	pr = "Tranquility server status: " + online + "." " Players online: " + players.text
+	return pr
+
+
+@hook.command('sisi')
+def sisistatus():
+	global sisicache, sisitree
+	now = arrow.utcnow()
+	if sisicache is 0 or now > sisicache:
+		url_si = requests.get("https://api.testeveonline.com/server/ServerStatus.xml.aspx")
+		tree = etree.fromstring(url_si.content)
+		sisicache = arrow.get(tree.find("cachedUntil").text)
+		sisitree = tree
+		print("Parsing SiSi server status. Seconds until cache:", (sisicache - now).seconds)
 
 	if tree.find("result/serverOpen").text == "True":
         	online = "Online"
 	else:
-		pr = "Server status: Offline."
+		pr = "Singularity server status: Offline."
 		return pr
 
 	players = tree.find("result/onlinePlayers")
-	pr = "Server status: " + online + "." " Players online: " + players.text
+	pr = "Singularity server status: " + online + "." " Players online: " + players.text
 	return pr
+
