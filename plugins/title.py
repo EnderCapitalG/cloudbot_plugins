7edca2b2dff458ca186400ddcbd8c2b2db392bff
EnderCapitalG <ender.capitalg@gmail.com>
2016-01-30 23:09:18 -0500

    Few changes, tossed html error codes 3XX to 5XX into a generic catchall with error message in console. Also very basic spam protection (doesn't allow a repeat url)

--- plugins/title.py
+++ plugins/title.py
@@ -7,18 +7,25 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 import time
 
+#basic spam protection
+lastURL = ''
+
 def titlep(url):
-	if url is "404" or url is "403" or url is "303":
+	global lastURL
+	if url is "failed":
 		return
+	if url in lastURL:
+		return
+	lastURL = url
 	parser = etree.HTMLParser(remove_blank_text=True)
 	with urlopen(url) as f:
 		tree = etree.parse(f, parser)
-	#TIL tree can return NoneType
 	tree = tree.getroot()
+	#apparently the tree can return nonetype as well?
 	if tree is None:
 		return
 	title = tree.find("head/title")
-	#test for NoneType and discard if so
+	#check for NoneType, discard if so
 	if title is None:
 		return
 	temp = title.text.replace('\n', ' ').replace('\r', ' ')
@@ -30,20 +37,22 @@ def resolve_redir(url):
 		return urlopen(url).geturl()
 	except HTTPError as e:
 		if e.code == 429:
-			time.sleep(4);
+			time.sleep(3);
 			return resolve_redir(url)
-		if e.code == 403:
-			return "403"
-		if e.code == 404:
-			return "404"
-		if e.code == 303:
-			return "303"
+		elif e.code < 600 or e.code > 300:
+			print("Titlec: url %s failed with code: %i" % (url, e.code))
+			return "failed"
 		raise
 
 @hook.regex(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
 def titlec(match, nick):
 	matchtext = match.group(0).lower()
-	#easy example of a filter (also keeps from clashing with twitch.py)
+	if 'SHODAN' in nick or 'Steve' in nick or 'TBot' in nick or 'ZyklonB' in nick:
+		return
 	if 'twitch.tv' in matchtext:
+		return
+	elif 'youtube.com' in matchtext or 'youtu.be' in matchtext:
+		return
+	elif 'twitter.com' in matchtext or 'https://t.co' in matchtext:
 		return
 	return titlep(resolve_redir(matchtext))
