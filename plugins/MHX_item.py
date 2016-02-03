082eed1eb944354dad294f69da93dc31231583b7
EnderCapitalG <ender.capitalg@gmail.com>
2016-02-02 23:55:48 -0500

    Few changes I should've done before first commit

--- plugins/MHX_item.py
+++ plugins/MHX_item.py
@@ -13,14 +13,10 @@ def build_table():
 	req = requests.get("http://kiranico.com/en/mhx/item").text
 	html = lxml.html.fromstring(req)
 	tt = html.xpath("//div[@class='col-lg-2 col-xs-6']/p/a/text()")
-	ht = html.xpath("//div[@class='col-lg-2 col-xs-6']/p/a/@href")
+	htmlTable = html.xpath("//div[@class='col-lg-2 col-xs-6']/p/a/@href")
 
 	for elem in tt:
 		textTable.append(elem.lower())
-
-	for elem in ht:
-		htmlTable.append(elem)
-
 
 @hook.command('mhx')
 def MHX_Item(text):
