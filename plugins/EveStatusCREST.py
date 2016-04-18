415bb4796fe180af79fba45b4ebf98b9d632ab77
EnderCapitalG <ender.capitalg@gmail.com>
2016-04-18 14:02:27 -0400

    Added a different Eve Status script, using the Eve CREST API (which is faster than the xml api). Also includes a DUST514 status to see how dead that game is

---
+++ plugins/EveStatusCREST.py
@@ -0,0 +1,53 @@
+#Made for cloudbot
+#Created by ender.capitalg@gmail.com
+
+from cloudbot import hook
+import requests
+
+@hook.command("eve")
+@hook.command("evestatus")
+@hook.command("tq")
+def tqstatus():
+	url_tq = "https://public-crest.eveonline.com/"
+	json = requests.get(url_tq).json()
+	if json is None:
+		return "Tranquility server status: Offline."
+
+	status = json["serviceStatus"]["eve"]
+	if "online" in status:
+		players = json["userCounts"]["eve_str"]
+		return "Tranquility server status: Online. Players online: " + players
+	
+	return "Tranqulity server status: Offline."
+
+@hook.command("dust")
+@hook.command("dust514")
+def dustplayers():
+	url_tq = "https://public-crest.eveonline.com/"
+	json = requests.get(url_tq).json()
+	if json is None:
+		return "Tranquility server status: Offline."
+
+	status = json["serviceStatus"]["dust"]
+	if "online" in status:
+		players = json["userCounts"]["dust_str"]
+		return "DUST514 players currently online: " + players
+
+	return "Tranquility server status: Offline."
+
+
+
+@hook.command("sisi")
+@hook.command("singularity")
+def sisistatus():
+	url_tq = "http://public-crest-sisi.testeveonline.com/"
+	json = requests.get(url_tq).json()
+	if json is None:
+		return "Singularity server status: Offline."
+
+	status = json["serviceStatus"]["eve"]
+	if "online" in status:
+		players = json["userCounts"]["eve_str"]
+		return "Singularity server status: Online. Players online: " + players
+	
+	return "Singularity server status: Offline."
