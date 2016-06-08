9a6154b3d2958e09600fcb3a8b4e393ae458786d
EnderCapitalG <ender.capitalg@gmail.com>
2016-06-08 19:58:06 -0400

    overhauled title.py into the new and improved Title.py using requests instead or urllib (and other bug fixes that were previously in title.py, stop using it)

---
+++ plugins/Title.py
@@ -0,0 +1,33 @@
+#Made for cloudbot
+#Created by ender.capitalg@gmail.com
+
+import requests
+from lxml.html import fromstring
+from cloudbot import hook
+
+#basic spam protection
+lastURL = ''
+
+header = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.97 Safari/537.36' }
+
+@hook.regex(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+def titlec(match, nick):
+	global lastURL, header
+	matchtext = match.group(0).lower()
+	#example filters, handled by other plugins
+	if 'twitch.tv' in matchtext or 'zkillboard' in matchtext:
+		return
+	
+	#to discard pictures
+	picture = matchtext.rsplit('.',1)[-1]
+	if 'jpg' in picture or 'png' in picture or 'gif' in picture or 'webm' in picture:
+		return
+
+	if matchtext in lastURL:
+		return
+	req = requests.get(match.group(0), headers=header)
+	tree = fromstring(req.content)
+	title = tree.findtext('.//title')
+	#there exist sites that literally have newlines in their titles because reasons
+	title = title.replace('\n', ' ').replace('\r', ' ')
+	return title
