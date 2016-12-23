f05b2dc58c656dced199d49e9a47525c2ebb8845
EnderCapitalG <ender.capitalg@gmail.com>
2016-12-23 10:08:45 -0500

    Overhaul of agdq.py that I worked on during the last SGDQ (summer 2016), finally decided to push it out. Also had to add in oauth support due to the changes to twitch's API (mostly due to them trying to combat viewbotting etc)

--- plugins/agdq.py
+++ plugins/agdq.py
@@ -1,23 +1,28 @@
 #Made for cloudbot
-#Taken from homero cloudbot plugins, updated and some parts fixed 
-#(the original was far from working correctly, at least for me)
+#Created by ender.capitalg@gmail.com
 
 from cloudbot import hook
-import json
+from cloudbot import blacklist
 import requests
 
 import lxml.html
 from datetime import datetime, timedelta
 import time
+import arrow
 
 schedule = []
 current_game = ''
 epoch = 0
 
+oauth_token = ""
+
 @hook.command('sgdq')
 @hook.command('agdq')
-def agdq():
-	url = "https://api.twitch.tv/kraken/streams/gamesdonequick"
+def agdq(nick, host):
+	global oauth_token
+	if blacklist.bl_ret(nick) or blacklist.bl_ret(host):
+		return
+	url = "https://api.twitch.tv/kraken/streams/gamesdonequick?oauth_token=" + oauth_token
 	obj = requests.get(url).json()
 	if obj['stream'] is None:
 		return "Stream is OFFLINE."
@@ -25,58 +30,92 @@ def agdq():
 	stream = "Twitch Streamer: gamesdonequick" + " -=- Status: Online -=- Game: " + obj['stream']['game'] + " -=- Title: " + obj['stream']['channel']['status'] + " -=- Viewers: " + str(obj['stream']['viewers'])
 	return stream + " Watch LIVE: http://www.twitch.tv/gamesdonequick"
 
-@hook.command('parse')
 def parse_agdq_schedule():
-	global schedule, current_game
-	obj = requests.get("https://api.twitch.tv/kraken/streams/gamesdonequick").json()
+	global schedule, current_game, oauth_token
+	obj = requests.get("https://api.twitch.tv/kraken/streams/gamesdonequick?oauth_token=" + oauth_token).json()
 	current_game = obj['stream']['game']
 
 	h_t = requests.get("https://gamesdonequick.com/schedule").text
 	html = lxml.html.fromstring(h_t)
-#After AGDQ2016, GDQ updated their table format (just before SGDQ)
-#	table = html.xpath("//tbody[@id='runTable']//tr[not(contains(@id, 'daySplit'))]")	
-#After SGDQ2016, GDQ once again updated their table format. I still don't know why (just before AGDQ 2017)
-#	table = html.xpath("//table[@id='runTable']//tbody//tr[not(contains(@id, 'day-split'))]")
 	table = html.xpath("//table[@id='runTable']//tbody//tr[not(contains(@class, 'second-row'))]")
+	table = table
 
 	gs = []
 	for element in table:
-		#set timedelta to your timezone relative to UTC
-		gtime = datetime.strptime(element.getchildren()[0].text, '%Y-%m-%dT%H:%M:%SZ') - timedelta(hours=5)
-		gtime = time.mktime(gtime.timetuple())
+		try:
+			gtime = arrow.get(element.getchildren()[0].text)
+		except arrow.parser.ParserError:
+			continue
 		game = element.getchildren()[1].text
 		gs.append([gtime, game])
 
 	schedule = gs
-	print("Updating AGDQ schedule.")
+	print("Updating AGDQ schedule. ")
 
 @hook.command('schedule')
-def sched():
+def sched(nick, host, text, notice):
+	global oauth_token
+	if blacklist.bl_ret(nick) or blacklist.bl_ret(host):
+		return
 	global schedule, current_game, epoch
 	#update game for comparison later
-	obj = requests.get("https://api.twitch.tv/kraken/streams/gamesdonequick").json()
-	current_game = obj['stream']['game']
+	obj = requests.get("https://api.twitch.tv/kraken/streams/gamesdonequick?oauth_token=" + oauth_token).json()
+	try:
+		current_game = obj['stream']['game']
+	except TypeError as e:
+		return "Stream offline."
 
-	#used to reparse the schedule in case of game overruns/short runs (1hr)
-	curtime = time.time()
-	timediff = curtime - epoch
-	if not schedule or timediff > 3600:
+	#used to reparse the schedule in case of game overruns/short runs (15min)
+	curtime = arrow.utcnow()
+	if epoch is 0:
+		#force update
+		timediff = 900
+	else:
+		timediff = (curtime - epoch).seconds
+	if not schedule or timediff > 900:
 		parse_agdq_schedule()
 		epoch = curtime
+
+	#if there's text following it, search in the game list and return time
+	if text:
+		for i, (tm, gm) in enumerate(schedule[1:]):
+			if text.lower() in gm.lower():
+				gtime = schedule[i+1][0].format('YYYY/M/D HH:mm:ss')
+				now = arrow.utcnow()
+				timetil = (schedule[i+1][0] - now).total_seconds()
+				time = timetil / 3600
+				hours = int(time)
+				minutes = int((time*60) % 60)
+				seconds = int((time*3600) % 60)
+				
+				return str(schedule[i+1][1]) + " " + str(gtime) + " UTC. Time until: " + "%d:%02d:%02d" % (hours, minutes, seconds)
+			#checking this is hackish right now, not bothered fixing (yet)
+			if i+2 is len(schedule):
+				notice("Game not found")
+				return
+
 	current = None
 	game = schedule[0][1]
 	for i, (tm, gm) in enumerate(schedule[1:]):
 		if current_game is gm:
 			current = gm
 			game = schedule[i+2][1]
-			return 'AGDQ Currently Playing: {} | Up Next: {}'.format(current, game)
+			return 'GDQ Currently Playing: {} | Up Next: {}'.format(current, game)
 		game = gm
 
 	#game didn't match, find game via time
-	now = time.time()
+	now = arrow.utcnow()
 	for i, (tm, gm) in enumerate(schedule[1:]):
 		if tm > now:
 			current = schedule[i][1]
 			game = gm
-			return 'AGDQ Currently Playing: {} | Up Next: {}'.format(current, game)
+			return 'GDQ Currently Playing: {} | Up Next: {}'.format(current, game)
 
+@hook.command('donations')
+def agdq_donation(nick, host):
+	if blacklist.bl_ret(nick) or blacklist.bl_ret(host):
+		return
+	h_t = requests.get("https://gamesdonequick.com/tracker/19").text
+	html = lxml.html.fromstring(h_t)
+	donation = html.xpath("//small")
+	return "Games Done Quick" + donation[0].text.replace('\n', ' ').replace('\r', ' ').replace('\u2014', '-')
