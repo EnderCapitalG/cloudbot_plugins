3d81d1eb62f106314442775f8ec394e5ddeb7bd9
EnderCapitalG <ender.capitalg@gmail.com>
2017-04-07 23:01:30 -0400

    Updates to fml, which was sorely out of date (and some bug fixes), since they changed their site layout etc

--- plugins/fml.py
+++ plugins/fml.py
@@ -2,6 +2,7 @@
 #Created by ender.capitalg@gmail.com
 
 from cloudbot import hook
+from cloudbot import blacklist
 import requests
 import lxml.html
 
@@ -17,17 +18,24 @@ def fml_parse():
 
 	req = requests.get("http://www.fmylife.com/random").text
 	html = lxml.html.fromstring(req)
-	table = html.xpath("//a[@class='fmllink']/text()")
+#	table = html.xpath("//a[@class='fmllink']/@href")
+#	table = html.xpath("//a[@class='fmllink']/text()")
+#	table = html.xpath("//p[@class='block-min']/a/text()")
+	table = html.xpath("//div[@class='panel-content']/p[@class='block']/a/text()")
+	
 
 	ar = []
 	for element in table:
-		ar.append(element)
+		ar.append(element.strip('\n').strip('\r'))
 		array_total += 1
 	fml_array = ar
+	print("fml_array updated.")
 
 
 @hook.command('fml')
-def fml():
+def fml(nick, host):
+	if blacklist.bl_ret(nick) or blacklist.bl_ret(host):
+		return
 	global fml_array, array_count, array_total
 	if not fml_array or array_count >= array_total:
 		fml_parse()
@@ -38,6 +46,17 @@ def fml():
 def ret_fml():
 	global array_count, array_total, fml_array
 
+#	retval = ''
+
+#	while "FML" not in fml_array[array_count]:
+#		retval = retval + fml_array[array_count]
+#		array_count += 1
+
 	retval = fml_array[array_count]
+
+	if retval is None or retval is "":
+		array_count += 1
+		retval = fml_array[array_count]
+
 	array_count += 1
 	return retval
